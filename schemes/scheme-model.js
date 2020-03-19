72732d2c997b03a1458b7c3134e0d2953fef4f38
fuston05 <fuston05@yahoo.com>
2020-03-18 20:03:24 -0400

    got findStep and add finished

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -16,15 +16,27 @@ function find(){
 function findById(id){
   return db('schemes')
   .where({id: id})
-  .first();
+  .first()
+  .then(res => {
+    if (res !== []){
+      return res;
+    }else{
+      return null;
+    }
+  })
 }//end find
 
-function findSteps(){
-
+function findSteps(id){
+  return db.select('steps.id', 'steps.step_number', 'schemes.scheme_name', 'steps.instructions')
+  .from('schemes')
+  .join('steps', 'steps.scheme_id', 'schemes.id')
+  .where({'schemes.id': id})
+  .orderBy('step_number');
 }//end find
 
-function add(){
-
+function add(schemeData){
+  return db('schemes')
+  .insert(schemeData);
 }//end find
 
 function update(){
